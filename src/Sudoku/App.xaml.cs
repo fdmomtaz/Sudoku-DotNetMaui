6dd6634853e1527bf6d6a7c30a5af60625912983
Farshad Momtaz <fdmomtaz@gmail.com>
2024-07-22 21:35:04 -0700

    Initial Version of the App

---
+++ src/Sudoku/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace Sudoku;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ src/Sudoku/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Sudoku;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
