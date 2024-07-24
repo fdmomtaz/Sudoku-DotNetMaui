189bbb2116ea782cb91dd72e7ceeb65e247eb2a7
Farshad Momtaz <fdmomtaz@gmail.com>
2024-07-23 21:04:14 -0700

    Finished the initial Sudoku UI

--- src/Sudoku/MainPage.xaml.cs
+++ src/Sudoku/MainPage.xaml.cs
@@ -1,24 +1,83 @@
-﻿namespace Sudoku;
+﻿using System.Windows.Input;
+
+namespace Sudoku;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
 
 	public MainPage()
 	{
 		InitializeComponent();
+
+		BindingContext = this;
+
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	int?[] sudokuArray = new int? [81] {
+		5, 3, 0, 0, 7, 0, 0, 0, null,
+		6, 0, 0, 1, 9, 5, 0, 0, null,
+		0, 9, 8, 0, 0, 0, 0, 6, 0,
+		8, 0, 0, 0, 6, 0, 0, 0, 3,
+		4, 0, 0, 8, 0, 3, 0, 0, 1,
+		7, 0, 0, 0, 2, 0, 0, 0, 6,
+		0, 6, 0, 0, 0, 0, 2, 8, 0,
+		0, 0, 0, 4, 1, 9, 0, 0, 5,
+		0, 0, 0, 0, 8, 0, 0, 7, 9
+	};
+
+	public int?[] SudokuArray
+    {
+        get => sudokuArray;
+        set
+        {
+            if (sudokuArray != value)
+            {
+                sudokuArray = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+	int? selectedCellRow;
+	public int? SelectedCellRow
+    {
+        get => selectedCellRow;
+        set
+        {
+            if (selectedCellRow != value)
+            {
+                selectedCellRow = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+	int? selectedCellColumn;
+	public int? SelectedCellColumn
+    {
+        get => selectedCellColumn;
+        set
+        {
+            if (selectedCellColumn != value)
+            {
+                selectedCellColumn = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+	
+
+	public ICommand SolveCommand => new Command<string>((cellNumber) =>
 	{
-		count++;
+		Int32.TryParse(cellNumber, out int cellNumberInt);
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+		int row = cellNumberInt / 9;
+		int col = cellNumberInt % 9;
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+		SelectedCellRow = row + 1 + row / 3;
+		SelectedCellColumn = col + 1 + col / 3;
+
+		OnPropertyChanged(nameof(SudokuArray));
+	});
 }
 
